4149c39d3c1e680238640ab6d51f006cca09d7d2
Jay C <cuellar.jay@gmail.com>
2018-07-13 16:35:06 -0700

    Finished 30/36 guns

---
+++ Assets/Resources/Scripts/EnemyWeaponSwap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyWeaponSwap : MonoBehaviour {
+
+    [Header("Arm sprites determined by equipped weapon:")]
+    public Sprite oneHandedWeaponArms;
+    public Sprite twoHandedWeaponArms;
+
+    SpriteRenderer armSpriteRenderer;
+
+    EnemyWeapon enemyWeaponScript;
+
+    void Start()
+    {
+        armSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        enemyWeaponScript = transform.GetComponentInChildren<EnemyWeapon>();
+    }
+
+    void FixedUpdate()
+    {
+        ArmSpriteSwap();
+    }
+
+    private void ArmSpriteSwap()
+    {
+        if (enemyWeaponScript.isTwoHanded)
+        {
+            armSpriteRenderer.sprite = twoHandedWeaponArms;
+        }
+        else
+        {
+            armSpriteRenderer.sprite = oneHandedWeaponArms;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/WeaponSwap.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class WeaponSwap : MonoBehaviour {
+
+    [Header("Arm sprites determined by equipped weapon:")]
+    public Sprite oneHandedWeaponArms;
+    public Sprite twoHandedWeaponArms;
+
+    SpriteRenderer armSpriteRenderer;
+
+    Weapon weaponScript;
+
+	void Start () {
+        armSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        weaponScript = GameObject.FindGameObjectWithTag("EquippedWeapon").GetComponent<Weapon>();
+	}
+	
+	void FixedUpdate ()
+    {
+        ArmSpriteSwap();
+    }
+
+    private void ArmSpriteSwap()
+    {
+        if (weaponScript.isTwoHanded)
+        {
+            armSpriteRenderer.sprite = twoHandedWeaponArms;
+        }
+        else
+        {
+            armSpriteRenderer.sprite = oneHandedWeaponArms;
+        }
+    }
+}
